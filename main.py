b516a0942d7904548ea80320693c6a6e20f76bb8
Gharsha1015 <gharshavardhan1015@gmial.com>
2026-07-04 14:09:45 +0530

    feat(stock-scanner): add cros

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.db.database import engine
 from app.db.base import Base
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.stock_scan import router as stock_router
 
@@ -8,6 +9,19 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(
     title="Stock Scanner API",
     version="1.0.0"
+)
+
+origins = [
+    "http://localhost:5173",      
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(stock_router)
